01982c91aa5d9ce784a71f69f86f760b3434f4d4
Arpan Pandey <arpan@hackersreboot.tech>
2023-05-06 22:19:17 +0530

    Change file names and stuff to be appropriate

--- backend/helpers/geo.py
+++ backend/helpers/geo.py
@@ -9,6 +9,7 @@ def lookup_lat_long(latitude: float, longitude: float):
     """
     This function takes a latitude and longitude and returns the location
     """
+    
     location = geolocator.reverse(f"{latitude}, {longitude}")
     return location.raw['address']
 
@@ -16,6 +17,7 @@ def get_continent(country_code: str) -> str:
     """
     This function takes a country code and returns the continent it is in
     """
+
     continent_code = pc.country_alpha2_to_continent_code(country_code)
     continent_dict = {
         "NA": "North America",
