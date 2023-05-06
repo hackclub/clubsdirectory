93aefec74fe0d611b3a2061af51676e33e0df5a1
Arpan Pandey <arpan@hackersreboot.tech>
2023-05-06 17:47:31 +0530

    Update metadata

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,20 @@ from helpers.classes import ClubElement, Leader
 load_dotenv()
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Clubs Directory API for Hack Club",
+    description="This API is used to get information about Hack Club's clubs and leaders",
+    version="0.0.1",
+    contact={
+        "name": "Holly",
+        "url": "https://hackclub.com",
+        "email": "holly@hackclub.com"
+    },
+    license_info={
+        "name": "MIT License",
+        "url": "https://opensource.org/licenses/MIT"
+    }
+)
 
 
 @app.get("/leaders")
