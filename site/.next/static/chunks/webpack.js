2f21fd2d013d6112790764f63d12289c7a1b077e
SerenityUX <thomas@serenidad.app>
2023-05-09 00:05:42 -0400

    middleWare fixed

--- site/.next/static/chunks/webpack.js
+++ site/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6c97e30303b627b1"; }
+/******/ 		__webpack_require__.h = function() { return "3dc8d83a16d423bf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- site/.next/static/webpack/webpack.40f5b078b86b4bb7.hot-update.js
+++ site/.next/static/webpack/webpack.40f5b078b86b4bb7.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "676f45165d598576"; }
+/******/ 	__webpack_require__.h = function() { return "7b5d1582370cdb48"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
