0c67d791d3ec00c9fb8d39a738d6d1b23fdc9c16
SerenityUX <thomas@serenidad.app>
2023-05-23 10:57:06 -0400

    casual bug fix, nothing to see here

--- site/components/ClubsTable.js
+++ site/components/ClubsTable.js
@@ -9,6 +9,7 @@ export const ClubsTable = ({
   isLoading,
   setSelectedClubs,
   selectedClubs,
+  selectedContinent,
   filterResults,
   sortResults,
   setClubOpened,
@@ -28,6 +29,13 @@ export const ClubsTable = ({
     <Container>
       {isLoading && selectedClubs.length == 0 ? <p>Loading...</p> : null}
     </Container>
+
+    
+    {
+      selectedContinent == "Antarctica" ?
+      <p>In a world dominated by the Outernet government, rumors spread like wildfire that Orpheus, the Hack Club Dinosaur, had sought refuge in the most absurd place imaginable—Antarctica! <br/><br/> Yes, that icy wasteland where penguins waddled and polar bears roamed, supposedly harbored the infamous hacker. As whispers of his frozen hideout grew louder, people couldn't help but chuckle at the idea of a giant dinosaur huddled among glaciers, furiously typing away on a laptop. <br/><br/> The image of Orpheus evading the Outernet government's grasp, camouflaged amidst the snow, became the talk of underground forums and encrypted networks. But hey, if anyone could outsmart those tech-savvy oppressors, perhaps a dinosaur with coding skills was just the unexpected hero the world needed. And who knows, maybe the penguins even joined forces, providing backup servers and tactical support while waddling around in stylish hacker gear. <br/><br/> The legend of Orpheus in Antarctica became a hilarious rallying cry, reminding everyone that in the strangest of places, rebellion and a touch of absurdity could bring about the most unexpected triumphs.</p> : null
+    }
+
     {clubs
 
       .filter((club) => filterResults(club))
